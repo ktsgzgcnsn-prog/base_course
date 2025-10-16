5bc49712d74495acc442d79ff224055678128897
Anfisa <ktsgzgcnsn@privaterelay.appleid.com>
2025-10-16 15:55:55 +0000

    моялаба

---
+++ task4.py
@@ -0,0 +1,7 @@
+a = [1, 5, "Good", "Red"]
+b = [9, "Blue", "Red", 1]
+print(a[1] + b[3])
+print(a[2] + b[2])
+print(a[0] * b[0])
+print(a[1] ** b[3])
+print(a + b)
---
+++ task6.py
@@ -0,0 +1,8 @@
+user_info = []
+user_info.append(input("Введите ваш возраст: 15"))
+user_info.append(input("Введите ваш пол: женский"))
+user_info.append(input("Введите ваше имя: Анфиса"))
+user_info.append(input("Введите ваш город: Калининград"))
+user_info.append(input("В каком классе вы учитесь: 10"))
+print('Собранная информация:')
+print(user_info)
